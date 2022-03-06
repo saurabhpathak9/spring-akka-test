57bf5a3b2b16898f5e1d4820025765dd48e69023
The Code Workload <the.code.workload@gmail.com>
2022-03-06 11:33:29 +0530

    Newer libraries

--- src/test/java/com/spring/akka/spring/akka/test/SpringAkkaTestApplicationTests.java
+++ src/test/java/com/spring/akka/spring/akka/test/SpringAkkaTestApplicationTests.java
@@ -1,29 +1,20 @@
 package com.spring.akka.spring.akka.test;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import scala.concurrent.duration.FiniteDuration;
-
-import java.util.concurrent.TimeUnit;
-
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.util.Timeout;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.boot.test.context.SpringBootTest;
 import scala.concurrent.Await;
 import scala.concurrent.Future;
 import scala.concurrent.duration.FiniteDuration;
 
+import java.util.concurrent.TimeUnit;
+
 import static akka.pattern.Patterns.ask;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringAkkaTestApplicationTests {
 
